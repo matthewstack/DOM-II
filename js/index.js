e68cadc4c1d579b3167ac1f59e6bd38f44697d21
Matthew Stack <matthewstack@Matthews-MacBook-Pro-4.local>
2021-08-31 15:45:22 -0400

    added 2 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+const h1Tag = document.querySelector("h1");
+
+const headerImg = document.querySelector(".intro img");
+
+h1Tag.addEventListener("mouseenter", (event) => {
+  event.target.style.backgroundColor = "red";
+  h1Tag.classList.add("grow");
+});
+
+headerImg.addEventListener("click", (event) => {
+  event.target.style.borderRadius = "25px";
+});
